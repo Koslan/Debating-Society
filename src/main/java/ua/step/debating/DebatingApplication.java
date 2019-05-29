b2b15190712141e3834d311161693183e1c8344d
Константин <@DESKTOP-V8E4228>
2019-05-29 21:27:57 +0300

    +

---
+++ src/main/java/ua/step/debating/DebatingApplication.java
@@ -0,0 +1,13 @@
+package ua.step.debating;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DebatingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DebatingApplication.class, args);
+	}
+
+}
