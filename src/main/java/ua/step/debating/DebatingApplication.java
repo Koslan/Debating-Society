902e683e79ca0a74521f0ba73412cef8de7cdc6a
Константин <@DESKTOP-V8E4228>
2019-05-29 21:32:06 +0300

    2

--- src/main/java/ua/step/debating/DebatingApplication.java
+++ src/main/java/ua/step/debating/DebatingApplication.java
@@ -8,6 +8,7 @@ public class DebatingApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DebatingApplication.class, args);
+		
 	}
 
 }
--- src/test/java/ua/step/debating/DebatingApplicationTests.java
+++ src/test/java/ua/step/debating/DebatingApplicationTests.java
@@ -9,6 +9,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @SpringBootTest
 public class DebatingApplicationTests {
 
+	
 	@Test
 	public void contextLoads() {
 	}
