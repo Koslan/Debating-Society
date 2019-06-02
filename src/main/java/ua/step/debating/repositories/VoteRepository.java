490cbb5ef434e1f562b6f4561aeba9628a3c2262
DimasikAlmazzik <chda98@bk.ru>
2019-06-02 13:26:18 +0300

    Отредактировал VoteRepository

--- src/main/java/ua/step/debating/repositories/VoteRepository.java
+++ src/main/java/ua/step/debating/repositories/VoteRepository.java
@@ -1,9 +1,8 @@
 package ua.step.debating.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import ua.step.debating.models.Vote;
 
-import ua.step.debating.models.User;
-
-public interface VoteRepository extends JpaRepository<User, Integer>  {
+public interface VoteRepository extends JpaRepository<Vote, Integer>  {
 
 }
