25162ed0c9d4fef940fd8a2940b12a2d3aa70b01
Pavel Bartalev <dasfootballspieler@gmail.com>
2019-06-02 13:11:10 +0300

    Сделал сущности theme & message

--- src/main/java/ua/step/debating/models/Message.java
+++ src/main/java/ua/step/debating/models/Message.java
@@ -1,5 +1,55 @@
 package ua.step.debating.models;
 
+import java.sql.Date;
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+
+
+
+
+@Entity
 public class Message {
 
+	private String text;
+	private Lobby Lobby;
+	private Date date;
+	private User author;
+	@ManyToMany
+	private List<User> listOfLikes;
+	
+	public String getText() {
+		return text;
+	}
+	public void setText(String text) {
+		this.text = text;
+	}
+	public Lobby getLobby() {
+		return Lobby;
+	}
+	public void setLobby(Lobby lobby) {
+		Lobby = lobby;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public User getAuthor() {
+		return author;
+	}
+	public void setAuthor(User author) {
+		this.author = author;
+	}
+	public List<User> getListOfLikes() {
+		return listOfLikes;
+	}
+	public void setListOfLikes(List<User> listOfLikes) {
+		this.listOfLikes = listOfLikes;
+	}
+
+	
+	
 }
--- src/main/java/ua/step/debating/models/Theme.java
+++ src/main/java/ua/step/debating/models/Theme.java
@@ -1,5 +1,56 @@
 package ua.step.debating.models;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+
+@Entity
 public class Theme {
 
+	private String name;
+	@ManyToOne
+	private List<Theme> subthemes; // подтемы
+	private User creator;
+	@OneToMany
+	private List<Lobby>  listOfDebats;  //Список  дебатов
+	@ManyToMany
+	private List<User>  subscribers;
+	
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public List<Theme> getSubthemes() {
+		return subthemes;
+	}
+	public void setSubthemes(List<Theme> subthemes) {
+		this.subthemes = subthemes;
+	}
+	public User getCreator() {
+		return creator;
+	}
+	public void setCreator(User creator) {
+		this.creator = creator;
+	}
+	public List<Lobby> getListOfDebats() {
+		return listOfDebats;
+	}
+	public void setListOfDebats(List<Lobby> listOfDebats) {
+		this.listOfDebats = listOfDebats;
+	}
+	public List<User> getSubscribers() {
+		return subscribers;
+	}
+	public void setSubscribers(List<User> subscribers) {
+		this.subscribers = subscribers;
+	}
+
+	
+	
 }
