6127fa0a6d8e2a3ed4a68f996df8a4a828e8bcac
Константин <@DESKTOP-V8E4228>
2019-06-08 23:05:57 +0300

    Добавил главную страницу и header

--- src/main/java/ua/step/debating/controllers/MainController.java
+++ src/main/java/ua/step/debating/controllers/MainController.java
@@ -5,17 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-import ua.step.debating.repositories.LobbyStatisticsRepository;
-import ua.step.debating.repositories.ThemeRepository;
+import ua.step.debating.repositories.SphereRepository;
 
 @Controller
 public class MainController {
 	@Autowired
-	private ThemeRepository themeRepositary;
+	private SphereRepository repoS;
 
 	@GetMapping("/")
 	private String getIndex(Model model) {
-		model.addAttribute("contentPage", "fragments/themes");
+		model.addAttribute("spheres", repoS.findAll());
+		model.addAttribute("contentPage", "fragments/header");
+		model.addAttribute("contentPage", "spheres");
 		return "index";
 	}
 }
