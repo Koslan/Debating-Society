5a77b62287f7e3781a5785960ad14b634a1a809c
DimasikAlmazzik <chda98@bk.ru>
2019-06-08 18:24:22 +0300

    data.sql
    Добавил файл data.sql. С помощью него добавил несколько значений в
    таблицу Сферы.

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO Sphere(id, name) VALUES (1, 'Политика');
+INSERT INTO Sphere(id, name) VALUES (2, 'Техника');
+INSERT INTO Sphere(id, name) VALUES (3, 'Творчество');
+INSERT INTO Sphere(id, name) VALUES (4, 'Кино');
+INSERT INTO Sphere(id, name) VALUES (5, 'Другое');
