833fd66b7aa75876eb0b141c4bca8aa6f80d6047
Pavel Bartalev <dasfootballspieler@gmail.com>
2019-06-09 15:35:03 +0300

    Выполнил вкладку theme рабочей

--- src/main/java/ua/step/debating/controllers/ThemeController.java
+++ src/main/java/ua/step/debating/controllers/ThemeController.java
@@ -1,10 +1,15 @@
 package ua.step.debating.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import ua.step.debating.models.Theme;
 import ua.step.debating.repositories.ThemeRepository;
 
 @Controller
@@ -17,6 +22,29 @@ public class ThemeController {
 	public String getTheme(Model model) {
 		model.addAttribute("theme", repoT.findAll());
 		return "theme";
+	}
+	
+	@GetMapping("/theme/add")
+	private String getAddPublisher(@ModelAttribute Theme theme, Model model) {
+		model.addAttribute("contentPage", "addTheme");
+		return "index";
+	}
+
+	@PostMapping("/theme/add")
+	private String addTheme(@ModelAttribute Theme theme) {
+		boolean isEmty = true;
+		List<Theme> publishers = repoT.findAll();
+		for (int i = 0; i < publishers.size(); i++) {
+			if (publishers.get(i).getName().equals(theme.getName())) {
+				isEmty = false;
+			}
+		}
+		if (isEmty) {
+			repoT.saveAndFlush(theme);
+			return "redirect:/theme";
+		} else {
+			return "redirect:/theme/add";
+		}
 	}
 	
 }
