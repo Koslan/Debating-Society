c8755d3f04006ec3d3790775aca7c9010a7dbc1a
Константин <@DESKTOP-V8E4228>
2019-06-11 00:55:34 +0300

    Сделал заготовку под создание дебата 1) изменил LobbyController(добавил метод createDebate)

--- src/main/java/ua/step/debating/controllers/LobbyController.java
+++ src/main/java/ua/step/debating/controllers/LobbyController.java
@@ -6,16 +6,28 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import ua.step.debating.repositories.LobbyRepository;
+import ua.step.debating.repositories.SphereRepository;
 
 @Controller
 public class LobbyController {
 
 	@Autowired
 	private LobbyRepository repoL;
+	
+	@Autowired
+	private SphereRepository repoS;
+
 
 	@GetMapping("/lobbies")
 	public String getLobbies(Model model) {
 		model.addAttribute("lobbies", repoL.findAll());
 		return "lobbies";
+	}
+	
+	@GetMapping("/createDebate")
+	public String createDebate(Model model) {
+		model.addAttribute("contentPage", "createDebate");
+		model.addAttribute("spheres", repoS.findAll());
+		return "index";
 	}
 }
