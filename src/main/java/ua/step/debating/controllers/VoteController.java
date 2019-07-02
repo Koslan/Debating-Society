c4fdb79511d2f3650b74b01113b556c3f3ae87a9
Pavel Bartalev <dasfootballspieler@gmail.com>
2019-07-02 14:39:44 +0300

    1) сделал логику голосования в VoteController

--- src/main/java/ua/step/debating/controllers/VoteController.java
+++ src/main/java/ua/step/debating/controllers/VoteController.java
@@ -4,7 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
+import ua.step.debating.models.Vote;
 import ua.step.debating.repositories.VoteRepository;
 
 @Controller
@@ -18,4 +21,12 @@ public class VoteController {
 		model.addAttribute("votes", repoV.findAll());
 		return "votes";
 	}
+	
+	@PostMapping("/debateLobby")
+	private String addVote(@ModelAttribute("Vote") Vote vote) {
+			repoV.saveAndFlush(vote);
+			return "redirect:/debateLobby";
+		
+	}
+	
 }
