fbb7c59d00d64fadf0339020aa8c862accf8c997
Константин <@DESKTOP-V8E4228>
2019-07-26 14:10:42 +0300

    +

--- src/main/java/ua/step/debating/WebSocketConfiguration.java
+++ src/main/java/ua/step/debating/WebSocketConfiguration.java
@@ -2,13 +2,13 @@ package ua.step.debating;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+//import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfiguration extends AbstractWebSocketMessageBrokerConfigurer  {
+public class WebSocketConfiguration { //extends AbstractWebSocketMessageBrokerConfigurer  {
 	
 	public void configureMessageBroker(MessageBrokerRegistry confi) {
 		confi.enableSimpleBroker("/debateLobby/1/chat");
